2cef04b11d71f0757526c6e23e2c24c179d8608f
Laura Andrade <devandrrade@gmail.com>
2025-03-03 12:17:41 -0300

    ajustando o titulo

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Styles from "./components/style.module.css";
 export default function page() {
   return (
     <div className={Styles.container}>
-      <h1 className={Styles.titulo}>Tabuleiro de damas</h1>
+      <h1 className={Styles.titulo}>Tabuleiro de Dama</h1>
       <Board />
     </div>
   );
